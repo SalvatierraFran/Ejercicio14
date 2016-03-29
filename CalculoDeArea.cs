4f4a1863e766213b5870bf0183995955bc187279
SalvatierraFran <mydesires5>
2016-03-29 00:30:39 -0300

    Creada la clase para calcular el area de cuadrado, triángulo y circulo.

---
+++ CalculoDeArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio14
+{
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double lado)
+        {
+            double resultado;
+
+            resultado = lado * lado;
+
+            return resultado;
+        }
+
+        public static double CalcularTriangulo(double Base1, double Altu)
+        {
+            double resultado;
+
+            
+        }
+    }
+}
